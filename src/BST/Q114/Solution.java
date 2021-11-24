52a4c69da2266ab548371463811e6cef8c6d4384
st2220 <sihan.tao20@imperial.ac.uk>
2021-11-24 07:14:15 -0800

    Solution for Q114. Flatten Binary Tree to Linked List

---
+++ src/BST/Q114/Solution.java
@@ -0,0 +1,27 @@
+package BST.Q114;
+
+import BST.TreeNode;
+
+public class Solution {
+  public void flatten(TreeNode root) {
+    if (root == null) {
+      return;
+    }
+
+    flatten(root.left);
+    flatten(root.right);
+
+    TreeNode left = root.left;
+    TreeNode right = root.right;
+
+    root.left = null;
+    root.right = left;
+
+    TreeNode p = root;
+
+    while (p.right != null) {
+      p = p.right;
+    }
+    p.right = right;
+  }
+}
