c719ea849c5ec07084d9a5bde0558fb66b762119
st2220 <sihan.tao20@imperial.ac.uk>
2021-11-26 03:38:25 -0800

    Fine, my fault

--- src/JPMorgan/Q91/Solution.java
+++ src/JPMorgan/Q91/Solution.java
@@ -5,17 +5,16 @@ public class Solution {
     int n = s.length();
     int[] f = new int[n + 1];
     f[0] = 1;
-    for (int i = 1; i < n + 1; i++) {
-      if (s.charAt(i - 1) != 0) {
+    for (int i = 1; i <= n; ++i) {
+      if (s.charAt(i - 1) != '0') {
         f[i] += f[i - 1];
       }
-      if (i >= 2
-          && s.charAt(i - 2) != 0
-          && (s.charAt(i - 2) - '0') * 10 + s.charAt(i - 1) - '0' <= 26) {
+      if (i > 1
+          && s.charAt(i - 2) != '0'
+          && ((s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0') <= 26)) {
         f[i] += f[i - 2];
       }
     }
-
     return f[n];
   }
 }
